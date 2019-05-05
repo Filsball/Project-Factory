af328b8ed8c4e928c24c11213b6af2fb55063547
MFeser <maxfeser98@web.de>
2019-05-05 19:55:47 +0200

    Zahnraeder_Scene und erste Scripte zu Zahnraedern

---
+++ Unity-Project/Project-Factory/Assets/Scripts/ZahnradAufsatz.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ZahnradAufsatz : MonoBehaviour
+{
+    public Zahnrad zahnrad;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        positionZahnrad();
+    }
+
+    void positionZahnrad()
+    {
+        if(zahnrad == null) { return; }
+
+        zahnrad.transform.position = transform.position + new Vector3(0, transform.localScale.y, 0);
+    }
+}
