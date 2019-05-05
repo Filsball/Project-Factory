f26ad71f2198b7e6ff5471b73afbe2c9ca78d44e
MFeser <maxfeser98@web.de>
2019-05-05 22:31:50 +0200

    Zahnräder_Aufsätze drehen sich

--- Unity-Project/Project-Factory/Assets/Scripts/ZahnradAufsatz.cs
+++ Unity-Project/Project-Factory/Assets/Scripts/ZahnradAufsatz.cs
@@ -5,6 +5,9 @@ using UnityEngine;
 public class ZahnradAufsatz : MonoBehaviour
 {
     public Zahnrad zahnrad;
+    public bool imUZSdrehen = true;
+
+    private float angle = 0;
     // Start is called before the first frame update
     void Start()
     {
@@ -15,12 +18,14 @@ public class ZahnradAufsatz : MonoBehaviour
     void Update()
     {
         positionZahnrad();
+        transform.Rotate(Vector3.up, (imUZSdrehen ? 1 : -1) * 100f * Time.deltaTime);
     }
 
     void positionZahnrad()
     {
         if(zahnrad == null) { return; }
 
-        zahnrad.transform.position = transform.position + new Vector3(0, transform.localScale.y, 0);
+        zahnrad.transform.position = transform.position + new Vector3(0, transform.localScale.y - zahnrad.transform.localScale.y/2, 0);
+        zahnrad.transform.rotation = transform.rotation;
     }
 }
--- Unity-Project/Project-Factory/Assets/Scripts/Zahnrad_Manager.cs
+++ Unity-Project/Project-Factory/Assets/Scripts/Zahnrad_Manager.cs
@@ -4,10 +4,19 @@ using UnityEngine;
 
 public class Zahnrad_Manager : MonoBehaviour
 {
+    private List<ZahnradAufsatz> aufsaetze = new List<ZahnradAufsatz>();
+
     // Start is called before the first frame update
     void Start()
     {
         
+        foreach(Transform child in transform)
+        {
+            if(child.GetComponent<ZahnradAufsatz>() != null)
+            {
+                aufsaetze.Add(child.GetComponent<ZahnradAufsatz>());
+            }
+        }
     }
 
     // Update is called once per frame
