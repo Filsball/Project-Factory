ed5fa1c7099d407dfefdef83f1aefadc72baba08
psowik2s <philipp.sowik@smail.inf.h-brs.de>
2019-07-01 02:32:00 +0200

    Türanimationen
    Türen öffen und schließen

---
+++ Unity-Project/Project-Factory/Assets/Kipp_Door.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+
+public class Kipp_Door : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void OnMouseOver()
+    {
+        if (Input.GetMouseButtonDown(0) && !GetComponent<Animation>().isPlaying)
+        {
+            PlayDoorAnim();
+        }
+    }
+    private int m_lastIndex = 0;
+
+    public void PlayDoorAnim()
+    {
+        if (!GetComponent<Animation>().isPlaying)
+        {
+            if (m_lastIndex == 0)
+            {
+                GetComponent<Animation>().Play("Kipp_Open");
+                m_lastIndex = 1;
+            }
+            else
+            {
+                GetComponent<Animation>().Play("Kipp_Close");
+                m_lastIndex = 0;
+            }
+        }
+    }
+}
---
+++ Unity-Project/Project-Factory/Assets/Schwarz_Door.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Schwarz_Door : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void OnMouseOver()
+    {
+        if (Input.GetMouseButtonDown(0) && !GetComponent<Animation>().isPlaying)
+        {
+            PlayDoorAnim();
+        }
+    }
+    private int m_lastIndex = 0;
+
+    public void PlayDoorAnim()
+    {
+        if (!GetComponent<Animation>().isPlaying)
+        {
+            if (m_lastIndex == 0)
+            {
+                GetComponent<Animation>().Play("Schwarz_Open");
+                m_lastIndex = 1;
+            }
+            else
+            {
+                GetComponent<Animation>().Play("Schwarz_Close");
+                m_lastIndex = 0;
+            }
+        }
+    }
+}
