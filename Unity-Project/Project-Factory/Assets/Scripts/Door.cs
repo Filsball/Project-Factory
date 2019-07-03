649a897dbcdac284e9dd922d66f1d4c5e80c65fa
Mfeser <maxfeser98@web.de>
2019-07-04 01:01:06 +0200

    Tür ist jetzt öffnen-/schließbar

--- Unity-Project/Project-Factory/Assets/Scripts/Door.cs
+++ Unity-Project/Project-Factory/Assets/Scripts/Door.cs
@@ -4,17 +4,16 @@ using UnityEngine;
 
 public class Door : InteractableObject
 {
-    public bool opening = false;
+    public bool animating = false;
+    public bool open = false;
     public float openingAnle = 50f;
     public float openingSpeed = 100f;
     public float curRot = 0f;
 
     public override void Interact()
     {
-        if (!opening)
-        {
-            opening = true;
-        }
+        open = !open;
+        animating = true;
     }
 
     public new void Start()
@@ -28,11 +27,21 @@ public class Door : InteractableObject
     {
         base.Update();
         curRot = transform.localRotation.eulerAngles.y;
-        if (opening)
+        if (animating)
         {
-            if (transform.localRotation.eulerAngles.y < 180 + openingAnle)
+            if (open)
             {
-                transform.Rotate(new Vector3(0, 0, openingSpeed) * Time.deltaTime);
+                if (transform.localRotation.eulerAngles.y < 180 + openingAnle)
+                {
+                    transform.Rotate(new Vector3(0, 0, openingSpeed) * Time.deltaTime);
+                }
+            }
+            else
+            {
+                if (transform.localRotation.eulerAngles.y > 180)
+                {
+                    transform.Rotate(new Vector3(0, 0, -openingSpeed) * Time.deltaTime);
+                }
             }
         }
     }
