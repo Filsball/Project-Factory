825a1f0623084b858fb1e51763547feb9f763ccb
Mfeser <maxfeser98@web.de>
2019-07-07 00:35:17 +0200

    Schwarzlicht-Raum texturiert (fehlende Normals und AO hinzugefügt) und kleine Anpassungen des Lichts, sodass Notizen lesbar sind

--- Unity-Project/Project-Factory/Assets/Scripts/Notiz.cs
+++ Unity-Project/Project-Factory/Assets/Scripts/Notiz.cs
@@ -4,6 +4,18 @@ using UnityEngine;
 [RequireComponent(typeof(Collider))]
 public class Notiz : InteractableObject
 {
+    public void ChangeLight(Light light)
+    {
+        bool currentlyActive = OilLightFaker.enabled;
+        DisableFakeLight();
+
+        OilLightFaker = light;
+        if (currentlyActive)
+        {
+            EnableFakeLight();
+        }
+    }
+
     public override void Interact()
     {
         RiddleInteract();
